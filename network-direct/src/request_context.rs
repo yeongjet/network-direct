f4e8c7e3d8deeedb2e1fe4b8639f910bab86382f
yeongjet <yeongjet@gmail.com>
2025-07-15 05:24:20 -0700

    update

--- network-direct/src/request_context.rs
+++ network-direct/src/request_context.rs
@@ -1,13 +1,7 @@
 use std::ffi::c_void;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct RequestContext(pub u128);
-
-impl Default for RequestContext {
-	fn default() -> Self {
-		Self(0)
-	}
-}
 
 impl RequestContext {
 	pub fn as_ptr(&self) -> *mut c_void {
